c2bac06bc2214ffacfde2530653b5ebd02aaacdc
Chris Folsom <cfols92@gmail.com>
2020-02-20 15:15:28 -0500

    added functionality to not retweet retweets

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,3 +1,5 @@
+# NOTE: follow this twitter bot @2021Gators on Twitter
+
 import tweepy
 import time
 
@@ -22,14 +24,41 @@ except:
 
 print("working...")
 
-since_id = 1230251765490233344
+# print(api.home_timeline()[0].id)
+
+# find the most recent id from tweets on the timeline
+since_id = api.home_timeline()[0].id
+
+# api.hometimeline() returns a list of status objects
+# to get the most recent one, access the first element in the list
+
+# initialize count to zero
+count = 0
 
 while True:
 
+    # loop through all tweets since the last "since_id"
     for tweet in api.home_timeline(since_id):
-        api.retweet(tweet.id)
-        since_id = tweet.id
+        
+        # add condition where the bot retweets only if the tweet is not a retweet
 
-    print("updated")
+        # try to retweet
+        try:
+            # do not retweet retweets
+            if "RT" not in tweet.text:
+                api.retweet(tweet.id)
+        
+        # if the tweet has already been retweeted, and error will be thrown
+        except:
+            print("Error was thrown because the tweet being retweeted has already been retweeted")
+            since_id = tweet.id
+            continue
+        
+        since_id = tweet.id
+        count = count + 1
+
+    print("updated\nfound " + str(count) + " new tweets")
+
+    count = 0
     
     time.sleep(60)
