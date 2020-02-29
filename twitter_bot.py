abb38040d91983a37195c31c87f3e110f5d2053f
Chris Folsom <cfols92@gmail.com>
2020-02-29 10:42:28 -0500

    prints exactly what is happening to console, added filter where tweet must have gained 20 favorites over the first 10 minutes

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,7 +2,7 @@
 
 import tweepy
 import time
-from datetime import datetime
+import datetime
 
 consumer_key = ''
 consumer_secret = ''
@@ -25,7 +25,7 @@ except:
 
 print("working...")
 
-# print(api.home_timeline()[0].id)
+# print(api.home_timeline()[0].user.screen_name)
 
 # find the most recent id from tweets on the timeline
 since_id = api.home_timeline()[0].id
@@ -38,30 +38,43 @@ while True:
     # initialize count to zero
     count = 0
 
+    print("There is/are " + str(len(api.home_timeline(since_id))) + " tweet(s) being processed")
+
     # loop through all tweets since the last "since_id"
     for tweet in api.home_timeline(since_id):
-        
-        # add condition where the bot retweets only if the tweet is not a retweet
 
-        # try to retweet
-        try:
-            # do not retweet retweets
-            # every retweet starts with "RT"
-            if "RT" not in tweet.text:
-                api.retweet(tweet.id)
-        
-        # if the tweet has already been retweeted, and error will be thrown
-        except:
-            print("Error was thrown because the tweet being retweeted has already been retweeted")
-            since_id = tweet.id
-            continue
-        
-        since_id = tweet.id
         count = count + 1
 
-    now = datetime.now()
+        # Retweet original tweets that have gained at least 20 favorites in the tweet's first 10 minutes
+
+        # create a condition here where the tweet must be over 10 minutes old
+        if datetime.datetime.utcnow() - tweet.created_at >= datetime.timedelta(0,10*60):
+
+            try:
+                # do not retweet retweets
+                # every retweet starts with "RT @"
+                if "RT @" not in tweet.text:
+                    if tweet.favorite_count >= 20: 
+                        api.retweet(tweet.id)
+                        print("Tweet " + str(count) + ": " + "\"" + tweet.text + "\"" + " from @" + tweet.user.screen_name + " was retweeted. Had " + str(tweet.favorite_count) + " favorite(s).")
+                    else:
+                        print("Tweet " + str(count) + ": " + "\"" + tweet.text + "\"" + " from @" + tweet.user.screen_name + " was not retweeted. Had " + str(tweet.favorite_count) + " favorite(s).")
+                else:
+                    print("Tweet " + str(count) + ": " + "\"" + tweet.text + "\"" + " from @" + tweet.user.screen_name + " is a retweet, so it will be ignored.")
+            # if the tweet has already been retweeted, and error will be thrown
+            except:
+                print("Error was thrown because the tweet being retweeted has already been retweeted")
+                since_id = tweet.id
+                continue
+            
+            since_id = tweet.id
+        else:
+            print("Tweet " + str(count) + ": " + "\"" + tweet.text + "\"" + " from @" + tweet.user.screen_name + " is less than 10 minutes old.")
+
+    now = datetime.datetime.now()
     current_time = now.strftime("%H:%M:%S")
 
-    print("updated at " + current_time + "\nfound " + str(count) + " new tweets")
+    print("updated at " + current_time)
+    print("____________________________________________________________________")
     
-    time.sleep(60)
+    time.sleep(60*2)
